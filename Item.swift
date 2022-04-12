bef3749bc85beb6a4d8946f64d9b7645a7a5519a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-04-12 17:20:27 -0300

    Modeling an Item

---
+++ Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  SPM_ImageGallery
+//
+//  Created by Luiz Araujo on 12/04/22.
+//
+
+import SwiftUI
+
+struct SwiftUI: Identifiable {
+    let id = UUID()
+    let url: URL
+    
+}
